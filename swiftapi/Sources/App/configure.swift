0940b79d23f819bee944e520bf5bbcfc98d1f049
Murilo Araujo <muernandes@icloud.com>
2020-11-08 16:23:22 -0300

    Changed params to env variables
    Signed-off-by: Murilo Araujo <muernandes@icloud.com>

--- swiftapi/Sources/App/configure.swift
+++ swiftapi/Sources/App/configure.swift
@@ -3,7 +3,12 @@ import Fluent
 import FluentPostgresDriver
 // configures your application
 public func configure(_ app: Application) throws {
-    app.databases.use(.postgres(hostname: "localhost", username: "lpp", password: "senha1234", database: "links"), as: .psql)
+    app.databases.use(.postgres(
+                        hostname: Environment.get("dbhost") ?? "localhost",
+                        username: Environment.get("dbusername") ?? "lpp",
+                        password: Environment.get("dbpassword") ?? "senha1234",
+                        database: Environment.get("dbdatabase") ?? "links"),
+                      as: .psql)
     app.migrations.add(CreateUsers())
     app.migrations.add(CreateLinks())
     try routes(app)
