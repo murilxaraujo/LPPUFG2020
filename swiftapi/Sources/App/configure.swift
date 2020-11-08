b05b4001eb4e9601e3d116c9f9cdff24c1019756
Murilo Araujo <muernandes@icloud.com>
2020-11-08 13:24:57 -0300

    implemented fluent
    Signed-off-by: Murilo Araujo <muernandes@icloud.com>

--- swiftapi/Sources/App/configure.swift
+++ swiftapi/Sources/App/configure.swift
@@ -1,10 +1,8 @@
 import Vapor
-
+import Fluent
+import FluentPostgresDriver
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    // register routes
+    app.databases.use(.postgres(hostname: "localhost:5432", username: "lpp", password: "senha1234", database: "links"), as: .psql)
     try routes(app)
 }
