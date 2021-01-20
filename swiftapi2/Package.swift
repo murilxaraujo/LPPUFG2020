a970aa9ac63beb457f443584b5944b3ef78ecff9
Murilo Araujo <muernandes@icloud.com>
2021-01-19 23:54:46 -0300

    Create second api
    Signed-off-by: Murilo Araujo <muernandes@icloud.com>

--- swiftapi2/Package.swift
+++ swiftapi2/Package.swift
@@ -7,19 +7,19 @@ let package = Package(
        .macOS(.v10_15)
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor-community/wkhtmltopdf.git", from: "4.0.0"),
+        .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.6.0")
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                "wkhtmltopdf",
+                "SwiftCSV"
             ],
             swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
                 .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
--- swiftapi2/Sources/App/routes.swift
+++ swiftapi2/Sources/App/routes.swift
@@ -1,11 +1,40 @@
 import Vapor
+import SwiftCSV
+import wkhtmltopdf
 
 func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
     }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    
+    app.on(.POST, "", body: .collect(maxSize: 20971520)) {
+        req -> EventLoopFuture<Response> in
+        let keys = "data;hora;ano;;titulo_eleicao;estado;;cod_cidade;cidade;;;nro_candidato;;;nome_candidato;cargo_disputado;;;;;;resultado;nro_partido_politico;sigla_partido_politico;nome_partido_politico;;coligacao;;;\n"
+        guard let file = req.body.string,
+              let csv = try? CSV(string: "\(keys)\(file)", delimiter: ";") else {
+            return req.eventLoop.makeFailedFuture(NSError(domain: "lpp", code: 0, userInfo: nil))
+        }
+        
+        let filteredList = csv.namedRows.filter { (item) -> Bool in
+            return item["cod_cidade"] == "93734" && item["cargo_disputado"] == "DEPUTADO ESTADUAL" && item["resultado"] == "ELEITO"
+        }
+        let filteredListNoRepeated = Set(filteredList)
+        var pageHtml = "<h1>Deputados Estaduais eleitos por Goiânia</h1><br><p>Nro          -     Nome Candidato       -    Partido<br>"
+        for item in filteredListNoRepeated {
+            pageHtml.append("\(item["nro_candidato"])      -     \(item["nome_candidato"])     -      \(item["sigla_partido_politico"])<br>")
+        }
+        pageHtml.append("</p>")
+        let page = Page(pageHtml)
+        let document = Document(margins:15)
+        document.pages = [page]
+        let pdf = document.generatePDF(on: req.application.threadPool, eventLoop: req.eventLoop)
+        return pdf.map { data in
+            return Response(
+                status: .ok,
+                headers: HTTPHeaders([("Content-Type", "application/pdf")]),
+                body: .init(data: data)
+            )
+        }
     }
+    
 }
